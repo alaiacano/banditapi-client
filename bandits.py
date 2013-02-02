97f69fc03f4be847c88517c695ba5534847a14e8
Adam Laiacano <adam@tumblr.com>
2013-02-02 13:22:03 -0500

    add test initialization methods

--- bandits.py
+++ bandits.py
@@ -40,4 +40,102 @@ class BanditAPI(object):
         params['reward'] = reward
         params['chosen_arm'] = arm
         params['test_id'] = self.test_id
-        return requests.post(self.UPDATE_ROUTE, params=params)
+        return requests.post(self.UPDATE_ROUTE, params=params).json
+
+    def epsilon_greedy(self, test_id, n_arms, epsilon):
+        """
+        Initializes an epsilon greedy test.
+
+        Inputs:
+        -------
+        test_id - string
+            The ID for the test.
+        n_arms - integer
+            The number of arms for the test.
+        epsilon - float between 0 and 1
+            Epsilon parameter.
+        """
+        n_arms = int(n_arms)
+        epsilon = float(epsilon)
+        if epsilon < 0.0 or epsilon > 1.0:
+            return
+        params = {
+            'algorithm': 'epsilon_greedy',
+            'test_id': test_id,
+            'n_arms': n_arms,
+            'epsilon': epsilon
+        }
+        return self._initialize_test(params)
+
+    def a_b(self, test_id, n_arms):
+        """
+        Initializes an A/B greedy test.
+
+        Inputs:
+        -------
+        test_id - string
+            The ID for the test.
+        n_arms - integer
+            The number of arms for the test.
+        """
+        n_arms = int(n_arms)
+        params = {
+            'algorithm': 'epsilon_greedy',
+            'test_id': test_id,
+            'n_arms': n_arms,
+            'epsilon': 0.0
+        }
+        return self._initialize_test(params)
+
+    def annealing_epsilon_greedy(self, test_id, n_arms):
+        """
+        Initializes an annealing epsilon greedy test.
+
+        Inputs:
+        -------
+        test_id - string
+            The ID for the test.
+        n_arms - integer
+            The number of arms for the test.
+        """
+        n_arms = int(n_arms)
+        params = {
+            'algorithm': 'annealing_epsilon_greedy',
+            'test_id': test_id,
+            'n_arms': n_arms,
+        }
+        return self._initialize_test(params)
+
+    def exp3(self, test_id, n_arms, gamma):
+        """
+        Initializes an EXP3 test.
+
+        Inputs:
+        -------
+        test_id - string
+            The ID for the test.
+        n_arms - integer
+            The number of arms for the test.
+        gamma - float between 0 and 1
+            Gamma parameter.
+        """
+        n_arms = int(n_arms)
+        gamma = float(gamma)
+        if gamma < 0.0 or gamma > 1.0:
+            return
+        params = {
+            'algorithm': 'exp3',
+            'test_id': test_id,
+            'n_arms': n_arms,
+            'gamma': gamma
+        }
+        return self._initialize_test(params)
+
+    def _initialize_test(self, params):
+        """
+        Initializes a test with the given test-specific parameters.
+        """
+        auth_params = self.__authenticate()
+        for k, v in auth_params:
+            params[k] = v
+        return requests.post(self.INIT_ROUTE, params=params).json
